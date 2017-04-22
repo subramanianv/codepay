1b300b9f494850f6787d2384c6602b496d96dcb1
Subramanian Venkatesan <subramanian.bsv@gmail.com>
2017-04-22 12:06:22 -0400

    WIP

--- app.js
+++ app.js
@@ -28,6 +28,8 @@ app.get("/", function(req, res) {
 	res.json("hello world");
 })
 
-app.listen(3000, function () {
+
+var port = process.env.PORT || 3000;
+app.listen(port, function () {
   console.log('Example app listening on port 3000!')
 });
