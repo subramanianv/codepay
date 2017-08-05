0306f15a07db167b8a94719c48bfd642bface234
lightswarm124 <lightswarm124@users.noreply.github.com>
2017-08-04 22:16:21 -0400

    Add files via upload
    Updated locking for token redemption

--- JobTracker.sol
+++ JobTracker.sol
@@ -1,16 +1,17 @@
-import "./SafeMath.sol";
-import "./Token.sol";
+pragma solidity ^0.4.4;
 
-pragma solidity ^0.4.8;
+import "./SafeMath.sol";
+import "./StandardToken.sol";
 
 contract JobTracker is SafeMath {
     event ChangeOwner(address oldOwner, address newOwner);
-    event Accept(address _jobWorker, uint _amount);
-    event Paid(address _jobWorker, uint _amount);
+    event AcceptWork (address projectManager, address jobWorker, uint amount);
 
     address public tokenContractAddress;
-    address public jobCreator = 0x0;
-    uint public endBlock;
+    StandardToken token;
+    address public jobCreator;
+    uint256 public lockBlockNumber;
+    uint256 public lockPayAmount;
 
     modifier onlyJobCreator {
         if (msg.sender == jobCreator) _;
@@ -18,31 +19,43 @@ contract JobTracker is SafeMath {
 
     function JobTracker(address _tokenContract) {
         jobCreator = msg.sender;
+        token = StandardToken(_tokenContract);
         tokenContractAddress = _tokenContract;
+        lockBlockNumber = 0;
+        lockPayAmount = 0;
     }
 
     function shareOf(address _jobWorker) constant returns (uint256) {
-        uint placeHolder = Token(tokenContractAddress).balanceOf(_jobWorker);
-        uint t_Supply = Token(tokenContractAddress).totalBalance();
+        uint256 placeHolder = StandardToken(tokenContractAddress).balanceOf(_jobWorker);
+        uint256 t_Supply = StandardToken(tokenContractAddress).totalBalance();
         return div(placeHolder * 1000, t_Supply);
     }
 
-    function acceptWork (address _jobWorker, uint _amount) onlyJobCreator returns (bool) {
-        Token(tokenContractAddress).approve(_jobWorker, _amount);
-        Accept(_jobWorker, _amount);
-        return Token(tokenContractAddress).transfer(_jobWorker, _amount);
+    function acceptWork (address _jobWorker, uint256 _amount) onlyJobCreator {
+        token.transfer(_jobWorker, _amount);
+        AcceptWork(msg.sender, _jobWorker, _amount);
     }
 
-    function payWorker (address _jobWorker) onlyJobCreator {
-        uint sendBalance = mul(this.balance, shareOf(_jobWorker));
-        Token(tokenContractAddress).transferFrom(jobCreator, _jobWorker, Token(tokenContractAddress).balanceOf(_jobWorker));
-        Paid(_jobWorker, this.send(sendBalance))
-        if(!this.send(sendBalance)) throw;
+    function payWorker (address _jobWorker) onlyJobCreator returns (bool) {
+        if (this.balance < lockPayAmount || block.number < lockBlockNumber) {
+          return false;
+        }
+        uint256 sendBalance = mul(this.balance, shareOf(_jobWorker));
+        sendBalance = div(sendBalance, 1000);
+        if (!_jobWorker.send(sendBalance)) throw;
+        token.approve(_jobWorker, jobCreator, token.balanceOf(_jobWorker));
+        token.transferFrom(_jobWorker, jobCreator, token.balanceOf(_jobWorker));
+        return true;
     }
 
     function changeJobCreator (address _newJobCreator) onlyJobCreator {
         ChangeOwner(jobCreator, _newJobCreator);
         jobCreator = _newJobCreator;
+    }
+
+    function lockPayment () onlyJobCreator {
+        lockPayAmount = this.balance;
+        lockBlockNumber = block.number;
     }
 
     function () payable {}
